3374df3602c6006cb9915cb5eb674df772a35c67
FRANCIS MICO A. ROSARIO <francismico.rosario@benilde.edu.ph>
2021-08-15 03:56:50 +0800

    Add temporary mapping for API information

--- src/main/java/com/example/nftindexer_backendspring/service/IndexerController.java
+++ src/main/java/com/example/nftindexer_backendspring/service/IndexerController.java
@@ -9,9 +9,10 @@ import java.util.Map;
 @RestController
 public class IndexerController {
 
-    @GetMapping("/version")
+    @GetMapping("/")
     public Map<String, String> test(){
         HashMap<String, String> hm = new HashMap<>();
+        hm.put("author", "FRANCIS MICO A. ROSARIO");
         hm.put("version", "0.1.0");
         return hm;
     }
